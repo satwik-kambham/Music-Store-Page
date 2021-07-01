aa77b2b7e01382362e6d60bd786f5d4a3d6edcc1
Satwik Kambham <satwik.kambham@gmail.com>
2021-07-01 18:16:34 +0530

    added edit profile option

--- script.js
+++ script.js
@@ -14,5 +14,26 @@ function login() {
 }
 
 function guestLogin() {
-  window.location.href = "profile.html"
+  window.location.href = "profile.html";
+}
+
+var editMode = false;
+function editProfile() {
+  let ids = ["Username", "Email", "Mobile", "Subscription"];
+  if (!editMode) {
+    ids.forEach((id) => {
+      let e = document.getElementById(id);
+      let text = e.innerText;
+      e.innerHTML = `<input value='${text}' id='${id + "1"}'/>`;
+    });
+    document.getElementById("editbtn").innerText = "Save Changes";
+  } else {
+    ids.forEach((id) => {
+      let e = document.getElementById(id + "1");
+      let text = e.value;
+      e.outerHTML = `${text}`;
+    });
+    document.getElementById("editbtn").innerText = "Edit Profile";
+  }
+  editMode = !editMode;
 }
